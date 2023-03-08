aa51087c4182e0119f5ba371f4688f949d6e2280
Anil Kumar Chavali <akchavali9@gmail.com>
2023-03-08 17:02:01 +0100

    adding parsing of config file

--- bme688_driver/bme688_driver.py
+++ bme688_driver/bme688_driver.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 
 import rclpy
+import yaml
+from ament_index_python.packages import get_package_share_directory
 from .gas_sensor import GasSensor
 
 
 def main():
+
+    # Read in parameter file
+    file_path = "/".join([get_package_share_directory("bme688_driver"),"sensor_config.yaml"])
+    with open(file_path, 'r') as stream:
+        sensor_config = yaml.full_load(stream)
+
+    # ROS init
     rclpy.init()
+
+    # Nodes and executors
     executor = rclpy.executors.SingleThreadedExecutor()
-    sensor_node = GasSensor()
+    sensor_node = GasSensor(kwargs=sensor_config)
     executor.add_node(sensor_node)
     executor.spin()
     executor.shutdown()
--- bme688_driver/gas_sensor.py
+++ bme688_driver/gas_sensor.py
@@ -6,13 +6,15 @@ from gas_sensor_msgs.msg import Data
 
 class GasSensor(Node):
 
-    def __init__(self, bus_id=5, freq=1):
+    def __init__(self, **kwargs):
 
+        config = list(kwargs.values())[0]
+        
         super().__init__('bme688_publisher')
         self.publisher_ = self.create_publisher(Data, '/bme688/data', 10)
 
         try:
-            self.sensor = bme680.BME680(bme680.I2C_ADDR_SECONDARY, i2c_bus=bus_id)
+            self.sensor = bme680.BME680(bme680.I2C_ADDR_SECONDARY, i2c_bus=config["i2c_bus"])
             self.sensor.set_humidity_oversample(bme680.OS_2X)
             self.sensor.set_pressure_oversample(bme680.OS_4X)
             self.sensor.set_temperature_oversample(bme680.OS_8X)
@@ -24,15 +26,14 @@ class GasSensor(Node):
                     self.get_logger().info(f"Name: {name}, Value: {value}")
             
             # these params can be tweaked for your use case
-            self.sensor.set_gas_heater_temperature(320)
-            self.sensor.set_gas_heater_duration(150)
-            self.sensor.select_gas_heater_profile(0)
+            self.set_gas_heater_params(config)
+            
 
         except Exception as e:
 
             self.get_logger().error(f"Sensor not initialized due to :  {e}")
         
-        timer_period = freq  # seconds
+        timer_period = 1/config["frequency"]  # seconds
         self.timer = self.create_timer(timer_period, self.publisher_callback)
     
     def publisher_callback(self):
@@ -56,3 +57,16 @@ class GasSensor(Node):
         
     def stamp_to_float(self, stamp):
         return stamp.sec + (stamp.nanosec * 1e-9)
+    
+    def set_gas_heater_params(self, config):
+
+        for param in config["gas_heater"]:
+            if list(param.key())[0] == "temperature":
+                self.sensor.set_gas_heater_temperature(param["temperature"])
+            if list(param.key())[0] == "duration":
+                self.sensor.set_gas_heater_duration(param["duration"])
+            if list(param.key())[0] == "profile":
+                self.sensor.select_gas_heater_profile(param["profile"])
+            print(list(param.key())[0])
+
+    
